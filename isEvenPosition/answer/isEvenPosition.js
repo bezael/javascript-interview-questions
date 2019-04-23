c796c32c0bde86c6c48374fc72953c923c3f62b6
Mustapha Aouas <mustapha.aouas@epitech.eu>
2019-04-23 13:42:51 +0200

    -- is even solution --

--- isEvenPosition/answer/isEvenPosition.js
+++ isEvenPosition/answer/isEvenPosition.js
@@ -1,11 +1,7 @@
 function isOnEvenPosition(table, value) {
-  for (let i = 0; i < table.length; i++) {
-    if (table[i] == value && i % 2 == 0) {
-      return true;
-    }
-  }
-  return false;
+  return table.findIndex(item => item == value) % 2 == 0;
 }
 
-console.log('not even:', isOnEvenPosition([5, 6, 2, 3], 6));
-console.log('even    :', isOnEvenPosition([5, 6, 2, 3], 2));
+console.log('not even   :', isOnEvenPosition([5, 6, 2, 3], 6));
+console.log('even       :', isOnEvenPosition([5, 6, 2, 3], 2));
+console.log('not present:', isOnEvenPosition([5, 6, 2, 3], 9));
