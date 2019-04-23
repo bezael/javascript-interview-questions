517026d3341e161322fbeeb7e80178c336c64cf4
Mustapha Aouas <mustapha.aouas@epitech.eu>
2019-04-23 15:32:07 +0200

    --

--- average/answer/average.js
+++ average/answer/average.js
@@ -1,7 +1,7 @@
 average = (...nums) => {
-  const somme = nums.reduce((accu, num) => accu + num);
-  return nums && nums.length ? somme / nums.length : 0;
+  return nums && nums.length ? nums.reduce((accu, num) => accu + num) / nums.length : 0;
 };
 
 console.log(average(2, 2, 2));
 console.log(average(0, 10, 5));
+console.log(average());
